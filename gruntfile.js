c27f8e910353494becf6824d77c215d46b167c5e
igormenezes343 <igorpm343@gmail.com>
2024-12-09 22:10:29 -0300

    commit inicial

--- gruntfile.js
+++ gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt){
                         },
                         {
                             match:'ENDERECO_DO_JS',
-                            replacement:'./src/scripts/main.min.js'
+                            replacement:'./scripts/main.min.js'
                         }
                     ]
                 },
@@ -87,7 +87,7 @@ module.exports = function(grunt){
         uglify:{
             target:{
                 files:{
-                    'dist/scripts/main.js': 'src/scripts/main.js'
+                    'dist/scripts/main.min.js':'src/scripts/main.js'
                 }
             }
         }
